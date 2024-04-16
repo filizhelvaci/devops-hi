388eb49395ac3e368b7fcd09339f4d0ed3066d27
filizhelvaci <filizhelvaci82@gmail.com>
2024-04-16 12:08:22 +0300

    first change has been made
    pom.xml
    .gitignore
    controller    added

---
+++ src/main/java/com/flz/devopshiApp.java
@@ -0,0 +1,13 @@
+package com.flz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class devopshiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(devopshiApp.class, args);
+	}
+
+}
